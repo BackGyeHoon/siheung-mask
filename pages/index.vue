eb9fc0c840672e25a63c904623bf1c148dc72a16
백계훈 <gh.back@designgj.com>
2020-03-10 15:10:30 +0900

    change branch

--- pages/index.vue
+++ pages/index.vue
@@ -72,15 +72,9 @@
 </template>
 
 <script>
-import Vue from "vue"
 import List from '~/components/layout/List'
-import Vuetify from "vuetify"
-
-const vuetifyOptions = {}
-Vue.use(Vuetify)
 
 export default {
-    vuetify: new Vuetify(vuetifyOptions),
     components: {
       List
     },
